893e9171ba56b1c71cf5593ef7f672d08580ecab
huihyeon06 <agnea0829@gmail.com>
2023-12-07 11:53:21 +0900

    [Chore] : 필요 시에 에러 발생

--- login.php
+++ login.php
@@ -8,11 +8,11 @@ $password = $_POST['password'];
 $sql = "select id from restaurant where uid = '$username' and password = '$password'";
 
 $result = mysqli_query($conn, $sql);
-if($result){
+if($result -> num_rows >0){
     echo "<script>alert('로그인 성공')</script>";
     echo "<meta http-equiv='refresh' content='0;url=main.html'>";
 }else{
-    echo "<script>alert('로그인 실패')</script>";
+    echo "<script>alert('아이디 또는 비밀번호가 틀렸습니다.')</script>";
     echo "<meta http-equiv='refresh' content='0;url=index.html'>";
 }
 
--- signup.php
+++ signup.php
@@ -9,19 +9,26 @@ $nickname = $_POST['nickname'];
 $gender = $_POST['gender'];
 $email = $_POST['email'];
 
-$sql = "insert into restaurant(uid, password, name, nickname, gender, email) values('$id', '$password', '$name', '$nickname', '$gender', '$email')";
-
+$sql = "select * from restaurant where $id";
 $result = mysqli_query($conn, $sql);
+if($result){
+    $sql = "insert into restaurant(uid, password, name, nickname, gender, email) values('$id', '$password', '$name', '$nickname', '$gender', '$email')";
+    $result = mysqli_query($conn, $sql);
 
-if ($result) {
-    echo "<script>alert('입력이 완료되었습니다');</script>";
-    echo "<meta http-equiv='refresh' content='0;url=index.html'>";
-} else {
-    echo "<script>alert('입력 중 오류가 발생하였습니다');</script>";
+    if ($result) {
+        echo "<script>alert('입력이 완료되었습니다');</script>";
+        echo "<meta http-equiv='refresh' content='0;url=index.html'>";
+    } else {
+        echo "<script>alert('입력 중 오류가 발생하였습니다');</script>";
+        echo "<meta http-equiv='refresh' content='0;url=index.html'>";
+    }
+    
+    mysqli_close();
+}else{
+    echo "<script>alert('이미 회원가입 된 아이디 입니다.');</script>";
     echo "<meta http-equiv='refresh' content='0;url=index.html'>";
 }
 
-mysqli_close();
 
 
 ?>
